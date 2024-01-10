d4d1a93beee0b037ef5d3dc0bbf81c0556ce0c6a
Pedro Santos <pedro.santos.personal@gmail.com>
2024-01-10 18:54:42 +0100

    refactor: symplify fibonacci implementation

--- lib/Fibonacci.ex
+++ lib/Fibonacci.ex
@@ -1,11 +1,6 @@
 defmodule Fibonacci do
-  def fibonacci(index) when index == 0 do
-    0
-  end
-
-  def fibonacci(index) when index == 1 do
-    1
-  end
+  def fibonacci(index) when index == 0, do: 0
+  def fibonacci(index) when index == 1, do: 1
 
   def fibonacci(index) do
     fibonacci(index - 1) + fibonacci(index - 2)
